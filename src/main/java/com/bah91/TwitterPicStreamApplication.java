21e75f1de09228ac4fb69def2c0b618cdef3a75f
bah91 <bah91@protonmail.com>
2017-05-04 19:16:33 +0100

    Initial import of Spring boot dependencies

---
+++ src/main/java/com/bah91/TwitterPicStreamApplication.java
@@ -0,0 +1,12 @@
+package com.bah91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterPicStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterPicStreamApplication.class, args);
+	}
+}
